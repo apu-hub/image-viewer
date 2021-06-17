7feb42eec9852b29c4c497bc5b1d2e9b2ed0e46d
Chayan Sarkar <59403799+apu-hub@users.noreply.github.com>
2021-06-17 16:15:13 +0530

    resize fixed
    1. Add out image resize code
    2. Clean unnecessary code and comments
    3. Add auto padding of Label code
    4.

--- src/main.py
+++ src/main.py
@@ -6,23 +6,47 @@ from os.path import isfile, exists, join, dirname
 from os import listdir
 
 
-# All Function
+# #######   All Function    #######
 # Show Image
 def show_image():
     global load_image
     global out_image
-    # global current_image_number
-    # global images_path
-    # Create Image Path And Load Image Before Used
-    load_image = ImageTk.PhotoImage(Image.open(join(images_path, filenames[current_image_number])))
-    out_image.grid_forget()
-    out_image = Label(image=load_image, width=1000, height=600)
-    out_image.grid(row=0, column=0, columnspan=3)
+    global temp_image
+    global out_image_width
+    global out_image_height
+    # print(images_path)  # Only For Dev
+    temp_image = Image.open(join(images_path, filenames[current_image_number]))  # Open Current Image
+    # Image Resize
+    if temp_image.width < temp_image.height:
+        # image Is Portrait
+        new_width = (temp_image.width / temp_image.height) * out_image_height
+        new_height = out_image_height
+        # print("Portrait OLD : " + str(temp_image.width) + " / " + str(temp_image.height))  # Only For Dev
+        # print("Portrait NEW : " + str(new_width) + " / " + str(new_height))  # Only For Dev
+    else:
+        # image Is Landscape
+        if (temp_image.width / temp_image.height) < (out_image_width / out_image_height):
+            new_width = (temp_image.width / temp_image.height) * out_image_height
+            new_height = out_image_height
+        else:
+            new_width = out_image_width
+            new_height = (temp_image.height / temp_image.width) * out_image_width
+        # print("Landscape OLD : " + str(temp_image.width) + " / " + str(temp_image.height))  # Only For Dev
+        # print("Landscape NEW : " + str(new_width) + " / " + str(new_height))  # Only For Dev
+    load_image = ImageTk.PhotoImage(temp_image.resize((int(new_width), int(new_height))))  # Load Image
+    out_image.grid_forget()  # Remove Old Label
+    out_image = Label(image=load_image)  # Set Image To Label
+    # Get Auto Padding For 'X' & 'Y' Axis
+    lable_pady = (out_image_height - new_height) / 2
+    lable_padx = (out_image_width - new_width) / 2
+    out_image.grid(row=0, column=0, columnspan=3, padx=lable_padx, pady=lable_pady)  # Show Label
+    # enter the padding value at the specified position, and leave the remaining as zero.
+    # It will give the padding to a widget from one side only,
+    # grid(padx=(padding from left side, padding from right side), pady=(padding from top, padding from bottom))
 
 
 # On Click Back Button
 def back_button():
-    # Get Access Of Global Variable
     global current_image_number
     if current_image_number == 0:  # If CIN Is 0
         current_image_number = len(filenames) - 1  # Show From Last
@@ -33,9 +57,8 @@ def back_button():
 
 # On Click Forward Button
 def forward_button():
-    # Get Access Of Global Variable
     global current_image_number
-    if current_image_number == len(filenames) - 1:  # If CIN Is == Total Files
+    if current_image_number == len(filenames) - 1:  # If CIN Is == Total Image Files
         current_image_number = 0  # Show From Start
     else:
         current_image_number = current_image_number + 1  # Show Next Image
@@ -50,32 +73,21 @@ def select_dir():
 
 # Get All Image Files From A Folder
 def get_all_image_files():
-    # Get Access Of Global Variable
     global images_path
-    # Loop Until Get Proper Directory
-    # temp = 1
-    # while temp:
     if exists(images_path):  # Is Directory Exists
         for file in listdir(images_path):  # Fetch All
-            if file.endswith(".png") or file.endswith(".jpg"):  # Filter File Type
+            if file.endswith(".png") or file.endswith(".jpg"):  # Filter Image File Type
                 filenames.append(file)  # Push Into 'filenames'
-
-    else:
-        # Load Empty Icon
-        images_path = dirname(__file__)
-        filenames.append("assets/images/empty.png")
-        # images_path = select_dir()  # Again Fetch Directory
 
 
 def open_button():
-    # Get Access Of Global Variable
     global images_path
     global filenames
     global current_image_number
     current_image_number = 0  # Show From Start
-    temp_var = select_dir()
+    temp_var = select_dir()  # Open File Explorer & Get Folder Select By User
     if temp_var:
-        images_path = temp_var  # Get Images Path
+        images_path = temp_var  # Set Images Path
         filenames = []  # Clean List For New Files
         get_all_image_files()
         show_image()
@@ -83,27 +95,37 @@ def open_button():
 
 # Auto Center Window
 def auto_center():
+    global root_width
+    global root_height
+    # Get Screen Width & Height
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-    root_width = 1000
-    root_height = 630
+    # Get 'X' & 'Y' Position Of Window
     x_position = int(screen_width / 2) - int(root_width / 2)
     y_position = int(screen_height / 2) - int(root_height / 2)
+    # Return Calculated Center Geometry
     return str(root_width) + "x" + str(root_height) + "+" + str(x_position) + "+" + str(y_position)
 
 
 # Windows SetUp
 root = Tk()
-root.title("Image Viewer 16-06-2021")
+root.title("Image Viewer 17-06-2021")  # Set Title Of Window
+# Set Root Window Width & Height
+root_width = 1000
+root_height = 630
 # Get Geometry
 root_geometry = auto_center()
 # parent_window.geometry("width_size x height_size + x_position + y_position")
-root.geometry(root_geometry)
-root.resizable(0, 0)
-if isfile("icon.ico"):
-    root.iconbitmap("icon.ico")
+root.geometry(root_geometry)  # Set Window Geometry
+root.resizable(0, 0)  # Stop Resize The Window
+
+# If ICON Available
+if isfile("assets/icon.ico"):
+    root.iconbitmap("assets/icon.ico")  # Set Window ICON
 
 # All Global Variable
+out_image_width = 990  # Out Image Width
+out_image_height = 600  # Out Image Height
 images_path = select_dir()  # Get Images Path
 current_image_number = 0  # Current Showing Image Number
 filenames = []  # To Store All Image Files
@@ -111,36 +133,33 @@ get_all_image_files()  # Get All Image From Image Folder
 
 # UI Designing
 # Create Image Path And Load Image Before Used
-load_image = ImageTk.PhotoImage(Image.open(join(images_path, filenames[current_image_number])))  # Default Image
+base_path = dirname(__file__)  # Get Software Base Path
+load_image = ImageTk.PhotoImage(Image.open(join(base_path, "assets/images/empty.png")))  # Get Image
 out_image = Label(image=load_image, width=1000, height=600)  # Set Image To Label
+out_image.grid(row=0, column=0, columnspan=3)
+
+# If User Select Any folder
 if images_path:
-    show_image()
-else:
-    out_image.grid(row=0, column=0, columnspan=4)
+    show_image()  # Call Show Image
 
 # Get All Icon For Buttons
-temp_image = Image.open(join(dirname(__file__), "assets/images/previous.png"))
-back_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))
+temp_image = Image.open(join(dirname(__file__), "assets/images/previous.png"))  # Get Image
+back_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))  # Resize And Set Image To Button
 
-temp_image = Image.open(join(dirname(__file__), "assets/images/open.png"))
-open_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))
+temp_image = Image.open(join(dirname(__file__), "assets/images/open.png"))  # Get Image
+open_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))  # Resize And Set Image To Button
 
-temp_image = Image.open(join(dirname(__file__), "assets/images/next.png"))
-forward_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))
+temp_image = Image.open(join(dirname(__file__), "assets/images/next.png"))  # Get Image
+forward_image = ImageTk.PhotoImage(temp_image.resize((30, 20)))  # Resize And Set Image To Button
 
 # Define Buttons
-button_back = Button(root, text="<<", image=back_image, command=back_button)
-# button_exit = Button(root, text="EXIT", command=root.quit)
-button_open = Button(root, text="OPEN", image=open_image, command=open_button)
-button_forward = Button(root, text=">>", image=forward_image, command=forward_button)
+button_back = Button(root, text="<<", image=back_image, command=back_button)  # Define Back Button
+button_open = Button(root, text="OPEN", image=open_image, command=open_button)  # Define Open Button
+button_forward = Button(root, text=">>", image=forward_image, command=forward_button)  # Define Forward Button
 
 # Put Buttons On Screen
-button_back.grid(row=1, column=0)
-# button_exit.grid(row=1, column=1)
-button_open.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_back.grid(row=1, column=0, padx="149")  # Show Back
+button_open.grid(row=1, column=1, padx="149")  # Show Open
+button_forward.grid(row=1, column=2, padx="149")  # Show Forward
 
 root.mainloop()
-
-# sticky = N+S+E+W can stretched the widgets
-# Anchor = N/S/E/W
